c3004b0323950fa01c6b40848a6b5838add37fe5
pumba <markhulia@users.noreply.github.com>
2017-08-08 16:36:14 +0200

    Minichat
    Chat between client/server
    Only 1 msg per repsonse from each side

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
-import socket
-import argparse. socket
-import datetime
+import argparse, socket
+from datetime import datetime
 
 MAX_BYTES = 65535
 
-def hostname(host):
-    addr = socket.gethostbyname(host)
-    return addr
 
 def server(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(('localhost'), port)
+    sock.bind(('localhost', port))
     print('Listening at {}'.format(sock.getsockname()))
     while True:
         data, address = sock.recvfrom(MAX_BYTES)
         text = data.decode('ascii')
-        print('The client das {} bytes long'.format(len(data)))
-        data.sendto(data, address)
+        print('The client at {} says {!r}'.format(address, text))
+        # text = 'Your data was {} bytes long'.format(len(data))
+        reply = input('Type your message: ')
+        data = reply.encode('ascii')
+        sock.sendto(data, address)
 
 def client(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    text = 'The time is {}'.format(datetime.now())
-    data = text.encode('ascii')
-    sock.sendto(('localhost', port))
-    print('The OS assigned me the address {}'.format(sock.getsockname()))
-    data, address = sock.recvfrom(MAX_BYTES)
-    text = data.decode
-    print('The server {} replied {!r}'.format(address,text))
+    while True:
+        text = input('Type your message: ')
+        data = text.encode('ascii')
+        sock.sendto(data, ('127.0.0.1', port))
+        print('The OS assigned me the address {}'.format(sock.getsockname()))
+        data, address = sock.recvfrom(MAX_BYTES)
+        text = data.decode('ascii')
+        print(text)
 
 if __name__ == '__main__':
-    print(hostname('www.google.com'))
+    choices = {'client':client,'server':server}
+    parser = argparse.ArgumentParser(description='Send and receive UDP locally')
+    parser.add_argument('role', choices=choices, help='Which role to play')
+    parser.add_argument('-p', metavar='PORT', type=int,default=1060,
+                        help='UDP port (default 1060)')
+    args = parser.parse_args()
+    function = choices[args.role]
+    function(args.p)
